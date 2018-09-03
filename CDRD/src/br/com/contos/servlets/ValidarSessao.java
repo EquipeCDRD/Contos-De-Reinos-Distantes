0ffa92b321f1df3dcf1b0cb47809bdcdaffb36e7
Kalyl Henings <kalylhenings@gmail.com>
2018-09-03 13:44:37 -0300

    Update ValidarSessao.java

--- CDRD/src/br/com/contos/servlets/ValidarSessao.java
+++ CDRD/src/br/com/contos/servlets/ValidarSessao.java
@@ -17,29 +17,22 @@ import br.com.contos.classes.Usuario;
 import br.com.contos.jdbc.JDBCUsuarioDAO;
 import br.com.contos.conexao.Conexao;
 
-/**
- * Servlet implementation class ValidarSessao
- */
 @WebServlet("/ValidarSessao")
 public class ValidarSessao extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ValidarSessao() {
         super();
-        // TODO Auto-generated constructor stub
     }
     
     private void process(HttpServletRequest request, HttpServletResponse response) 
     		throws ServletException, IOException {
-    	System.out.println("validando sess�o");
+    	System.out.println("validando sessão");
     	try {
     		HttpSession sessao = request.getSession();
     		
     		if (request.getParameter("p").equals(sessao.getAttribute("permissao"))) {
-    			System.out.println("sess�o OK");
+    			System.out.println("sessão OK");
     			Conexao conec = new Conexao();
         		Connection conexao = conec.abrirConexao();
         		JDBCUsuarioDAO jdbcUsuario = new JDBCUsuarioDAO(conexao); 
@@ -63,19 +56,11 @@ public class ValidarSessao extends HttpServlet {
     	}  		
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		process(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		process(request, response);
 	}
 
