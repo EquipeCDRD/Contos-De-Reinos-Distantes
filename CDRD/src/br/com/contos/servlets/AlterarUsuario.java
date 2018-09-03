ff234fe733166ec79bc184a2d72ace24a1fae308
Kalyl Henings <kalylhenings@gmail.com>
2018-09-03 13:33:42 -0300

    Update AlterarUsuario.java

--- CDRD/src/br/com/contos/servlets/AlterarUsuario.java
+++ CDRD/src/br/com/contos/servlets/AlterarUsuario.java
@@ -19,19 +19,11 @@ import br.com.contos.jdbc.JDBCUsuarioDAO;
 
 import com.google.gson.Gson;
 
-/**
- * Servlet implementation class AlterarUsuario
- */
 @WebServlet("/AlterarUsuario")
 public class AlterarUsuario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public AlterarUsuario() {
         super();
-        // TODO Auto-generated constructor stub
     }
     
     private void process(HttpServletRequest request, HttpServletResponse response)
@@ -79,19 +71,11 @@ public class AlterarUsuario extends HttpServlet {
     }
     
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		process(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		process(request, response);
 	}
 
