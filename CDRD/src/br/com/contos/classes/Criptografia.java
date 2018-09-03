26acad801e8e1fca100217c442bbd1f0c07e9f11
Kalyl Henings <kalylhenings@gmail.com>
2018-09-03 13:26:46 -0300

    Update Criptografia.java

--- CDRD/src/br/com/contos/classes/Criptografia.java
+++ CDRD/src/br/com/contos/classes/Criptografia.java
@@ -21,7 +21,6 @@ public class Criptografia {
             senhaCriptografada = hexString.toString();
             
         } catch (Exception e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
             }
             return senhaCriptografada;
