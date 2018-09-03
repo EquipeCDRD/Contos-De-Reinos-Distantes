92b739119a152e849c81a986f0f1a669014f30a5
Kalyl Henings <kalylhenings@gmail.com>
2018-09-03 13:47:17 -0300

    Update BuscarUsuario.java

--- CDRD/src/br/com/contos/servlets/BuscarUsuario.java
+++ CDRD/src/br/com/contos/servlets/BuscarUsuario.java
@@ -15,19 +15,12 @@ import br.com.contos.classes.Usuario;
 import br.com.contos.conexao.Conexao;
 import br.com.contos.jdbc.JDBCUsuarioDAO;
 
-/**
- * Servlet implementation class BuscarUsuario
- */
 @WebServlet("/BuscaUsuario")
 public class BuscarUsuario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public BuscarUsuario() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -49,19 +42,11 @@ public class BuscarUsuario extends HttpServlet {
     	}
     }
     
-    /**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		process(request, response);	
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		process(request, response);
 	}
 
