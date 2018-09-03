50f3b25f19d6264741bf7844dfe7e680b1f953bb
Kalyl Henings <kalylhenings@gmail.com>
2018-09-03 15:16:47 -0300

    Add files via upload

--- CDRD/src/br/com/contos/servlets/AlterarNotificacao.java
+++ CDRD/src/br/com/contos/servlets/AlterarNotificacao.java
@@ -1,70 +1,63 @@
 package br.com.contos.servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.sql.Connection;
-import java.util.Map;
-import java.util.HashMap;
-import com.google.gson.Gson;
-
-import java.time.format.DateTimeFormatter;  
-import java.time.LocalDateTime;    
-
 import br.com.contos.classes.Notificacao;
 import br.com.contos.jdbc.JDBCNotificacaoDAO;
 import br.com.contos.conexao.Conexao;
 
-/**
- * Servlet implementation class InsereNotificacao
- */
-@WebServlet("/InserirNotificacao")
-public class InserirNotificacao extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+import com.google.gson.Gson;
 
+/**
+ * Servlet implementation class AtualizarNotificacao
+ */
+@WebServlet("/AlterarNotificacao")
+public class AlterarNotificacao extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
     /**
-     * Default constructor. 
+     * @see HttpServlet#HttpServlet()
      */
-    public InserirNotificacao() {
+    public AlterarNotificacao() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-    private void process(HttpServletRequest request, HttpServletResponse response)
-    		throws ServletException, IOException{
-    	
-    	Notificacao notificacao = new Notificacao();
-    	
-    	try	{
-    		notificacao.setNotificacao(request.getParameter("notificacao"));
-    		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-    		LocalDateTime now = LocalDateTime.now();  
-    		notificacao.setDataCriacao(dtf.format(now));
-    		notificacao.setUsuarioId(request.getParameter("usuario_id"));
+    private void process(HttpServletRequest request, HttpServletResponse response) 
+    		throws ServletException, IOException {
+    	try {		
     		Conexao conec = new Conexao();
     		Connection conexao = conec.abrirConexao();
     		JDBCNotificacaoDAO jdbcNotificacao = new JDBCNotificacaoDAO(conexao);
     		Map<String, String> msg = new HashMap<String, String>();
-    		boolean retorno = jdbcNotificacao.inserir(notificacao);
-    		if(retorno){
-    			msg.put("msg", "Notificação enviada com sucesso.");
-    		}else{
-    			msg.put("msg", "Não foi possível enviar a notificação.");
-    		}
+    		Notificacao notificacao = new Notificacao();
+    		notificacao.setNotificacao(request.getParameter("txaeditnotificacao"));
+    	    notificacao.setId(request.getParameter("idnotificacao"));
+    	    boolean retorno = jdbcNotificacao.alterar(notificacao);
+    		conec.fecharConexao();    	
+    			if (retorno) {
+    				msg.put("msg", "Notificação editada com sucesso.");
+    		    } else {
+    		    	msg.put("msg", "Não foi possível editar a notificação.");
+    		    	msg.put("erro", "true");
+    		    }
     		conec.fecharConexao();
-	    	System.out.println(msg);
-    		String json = new Gson().toJson(msg);
     		
     		response.setContentType("application/json");
-    		response.setCharacterEncoding("UTF-8");
-    		response.getWriter().write(json);
-    	}catch (IOException e) {
+    	   	response.setCharacterEncoding("UTF-8");
+    	   	response.getWriter().write(new Gson().toJson(msg));
+    	} catch (IOException e) {
     		e.printStackTrace();
-    	}  		
+    	}
     }
     
 	/**
---
+++ CDRD/src/br/com/contos/servlets/BuscarNotificacao.java
@@ -0,0 +1,68 @@
+package br.com.contos.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import br.com.contos.classes.Notificacao;
+import br.com.contos.jdbc.JDBCNotificacaoDAO;
+import br.com.contos.conexao.Conexao;
+
+/**
+ * Servlet implementation class BuscarNotificacao
+ */
+@WebServlet("/BuscarNotificacao")
+public class BuscarNotificacao extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BuscarNotificacao() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	List<Notificacao> notificacoes = new ArrayList<Notificacao>();
+    	Conexao conec = new Conexao();
+    	Connection conexao = conec.abrirConexao();
+    	JDBCNotificacaoDAO jdbcNotificacao = new JDBCNotificacaoDAO(conexao);
+    	notificacoes = jdbcNotificacao.buscar();
+    	conec.fecharConexao();
+    	String json = new Gson().toJson(notificacoes);
+    	try {
+    		response.setContentType("application/json");
+    		response.setCharacterEncoding("UTF-8");
+    		response.getWriter().write(json);
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		process(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		process(request, response);
+	}
+
+}
