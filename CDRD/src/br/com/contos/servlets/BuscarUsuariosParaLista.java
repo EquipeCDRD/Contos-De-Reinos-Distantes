f1ee87356ce21b429f11a6d4783c8d9219259660
Kalyl Henings <kalylhenings@gmail.com>
2018-09-03 13:49:28 -0300

    Update BuscarUsuariosParaLista.java

--- CDRD/src/br/com/contos/servlets/BuscarUsuariosParaLista.java
+++ CDRD/src/br/com/contos/servlets/BuscarUsuariosParaLista.java
@@ -16,19 +16,13 @@ import com.google.gson.Gson;
 import br.com.contos.classes.Usuario;
 import br.com.contos.conexao.Conexao;
 import br.com.contos.jdbc.JDBCUsuarioDAO;
-/**
- * Servlet implementation class BuscarUsuarioParaLista
- */ 
+
 @WebServlet("/BuscarUsuariosParaLista")
 public class BuscarUsuariosParaLista extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public BuscarUsuariosParaLista() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -53,19 +47,11 @@ public class BuscarUsuariosParaLista extends HttpServlet {
     	
     }
     
-    /**
-   	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-   	 */
    	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-   		// TODO Auto-generated method stub
    		process(request, response);	
    	}
 
-   	/**
-   	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-   	 */
    	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-   		// TODO Auto-generated method stub
    		process(request, response);
    	}
 
