1f3e6f3ea5990e73a353bebf0d0398f0a743e9a2
Kalyl Henings <kalylhenings@gmail.com>
2018-09-03 13:36:31 -0300

    Update InserirNotificacao.java

--- CDRD/src/br/com/contos/servlets/InserirNotificacao.java
+++ CDRD/src/br/com/contos/servlets/InserirNotificacao.java
@@ -19,19 +19,12 @@ import br.com.contos.classes.Notificacao;
 import br.com.contos.jdbc.JDBCNotificacaoDAO;
 import br.com.contos.conexao.Conexao;
 
-/**
- * Servlet implementation class InsereNotificacao
- */
 @WebServlet("/InserirNotificacao")
 public class InserirNotificacao extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
     public InserirNotificacao() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     private void process(HttpServletRequest request, HttpServletResponse response)
@@ -69,19 +62,11 @@ public class InserirNotificacao extends HttpServlet {
     	}  		
     }
     
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		process(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		process(request, response);
 	}
 
