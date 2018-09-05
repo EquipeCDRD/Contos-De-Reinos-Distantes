667fd71b7d5dbb195d5d2c93947959891110ec00
Kalyl Henings <kalylhenings@gmail.com>
2018-09-05 00:21:57 -0300

    Update BuscarNotificacao.java

--- CDRD/src/br/com/contos/servlets/BuscarNotificacao.java
+++ CDRD/src/br/com/contos/servlets/BuscarNotificacao.java
@@ -17,19 +17,12 @@ import br.com.contos.classes.Notificacao;
 import br.com.contos.jdbc.JDBCNotificacaoDAO;
 import br.com.contos.conexao.Conexao;
 
-/**
- * Servlet implementation class BuscarNotificacao
- */
 @WebServlet("/BuscarNotificacao")
 public class BuscarNotificacao extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public BuscarNotificacao() {
         super();
-        // TODO Auto-generated constructor stub
     }
     
     private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -49,17 +42,12 @@ public class BuscarNotificacao extends HttpServlet {
     	}
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		process(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		process(request, response);
