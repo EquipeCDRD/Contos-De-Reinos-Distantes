9ca4f605e533e36ed9c2bea638a294f352d9ad37
A bug <39497013+a-bug@users.noreply.github.com>
2018-10-03 19:58:27 -0300

    tentativa de desarruinar tudo
    socorro

--- CDRD/WebContent/resources/js/inicioServlet.js
+++ CDRD/WebContent/resources/js/inicioServlet.js
@@ -5,19 +5,19 @@
  * 			  processamento da requisição.
  */
 
-$(document).ready(function () {
+ $(document).ready(function(){
 
 	//Pesquisa e construção da tabela do ranking
-	$(function () {
+	$(function(){
 		var html = "<table id='tabela'> <!--Tabela de Ranking-->"
-			+ "<tr>"
-			+ "<th colspan='3' class='tdTabela'><h2 class='tituloTabela'>MELHORES GUERREIROS DO REINO</h2></th>"
-			+ "</tr>"
-			+ "<tr>"
-			+ "<th class='tdTabela'>Posição</th>"
-			+ "<th class='tdTabela'>Usuário</th>"
-			+ "<th class='tdTabela'>Pontuação</th>"
-			+ "</tr>"
+				 + "<tr>"
+				 +   "<th colspan='3' class='tdTabela'><h2 class='tituloTabela'>MELHORES GUERREIROS DO REINO</h2></th>"
+				 + "</tr>"
+				 + "<tr>"
+				 +	"<th class='tdTabela'>Posição</th>"
+				 +	"<th class='tdTabela'>Usuário</th>"
+				 +	"<th class='tdTabela'>Pontuação</th>"
+				 + "</tr>"
 		$.ajax({
 			type: "POST",
 			url: "BuscarPontuacao",
@@ -27,83 +27,83 @@ $(document).ready(function () {
 				html += geraTabelaRanking(dados);
 				console.log(html);
 				$("#containerRankingIndex").html(html);
-			},
-			error: function (info) {
+				},
+			error:function (info){
 				html += "<tr>"
-					+ "<td class='tdTabela'> Erro ao carregar o ranking: " + info.status + " - " + info.statusText + "</td>"
-					+ "</tr>"
+					  + 	"<td class='tdTabela'> Erro ao carregar o ranking: " + info.status + " - " + info.statusText + "</td>"
+					  + "</tr>"
 				console.log(html);
 			}
 		})
 	})
-
-	geraTabelaRanking = function (listaRanking) {
+	
+	geraTabelaRanking = function(listaRanking){
 		var dados = "";
-		if (listaRanking != undefined && listaRanking.length > 0) {
-			for (var i = 0; i < listaRanking.length; i++) {
+		if (listaRanking != undefined && listaRanking.length > 0){
+			for (var i=0; i<listaRanking.length; i++){
 				dados += "<tr>" +
-					"<td class='tdTabela'><span>" + listaRanking[i].posicaoRanking + "</span></td>" +
-					"<td class='tdTabela'><span>" + listaRanking[i].nomeDeUsuario + "</span></td>" +
-					"<td class='tdTabela'><span>" + listaRanking[i].score + "</span></td>" +
-					"</tr>"
+						"<td class='tdTabela'><span>"+listaRanking[i].posicaoRanking+"</span></td>" +
+						"<td class='tdTabela'><span>"+listaRanking[i].nomeDeUsuario	+"</span></td>" +
+						"<td class='tdTabela'><span>"+listaRanking[i].score+"</span></td>" +
+						"</tr>"
 			}
-		} else if (listaRanking == "") {
+		} else if (listaRanking == ""){
 			dados += "<tr><td colspan='2'>Nenhum registro encontrado</td></tr>";
 		}
 		return dados;
 	}
 	//======================================================
-	//funções de login, cadastro e recuperação de senha
+		//funções de login, cadastro e recuperação de senha
 	//======================================================
-
-	login = function () {
+	
+	login = function(){
 		var login = $("input[name=txtlogin]").val();
 		var pwdsenhalogger = $("input[name=pwdsenhalogger]").val();
 		$.ajax({
 			type: "POST",
 			url: "Login",
-			data: "login=" + login + "&senha=" + pwdsenhalogger,
+			data: "login="+login+"&senha="+pwdsenhalogger,
 			success: function (msg) {
-				if (msg.msg != null)
+				if (msg.msg!=null)
 					alert(msg.msg);
 				else
 					window.location.href = msg.url;
 			},
 			error: function (info) {
-				alert("Erro ao tentar login: " + info.status + " - " + info.statusText);
+				alert("Erro ao tentar login: "+ info.status + " - " + info.statusText);		   
 			}
 		});
 	}
-
-	cadastrar = function () {
+	
+	cadastrar = function(){
 		$.ajax({
 			type: "POST",
 			url: "InserirUsuario",
-			data: $("#cadastrarJogador").serialize() + "&p=1",
+			data: $("#cadastrarJogador").serialize()+"&p=1",
 			success: function (msg) {
 				console.log(msg);
 				alert(msg.msg);
 				$(this).delay(5000).location.reload();
 			},
 			error: function (info) {
-				alert("Erro ao cadastrar novo jogador: " + info.status + " - " + info.statusText);
+				alert("Erro ao cadastrar novo jogador: "+ info.status + " - " + info.statusText);
 			}
 		});
 	}
 
-	recuperarSenha = function () {
+	recuperarSenha = function (){
 		var email = $("input[name=txtmail]").val();
 		$.ajax({
-			type: "POST",
+			type:"POST",
 			url: "RecuperarSenha",
-			data: "txtmail=" + email,
-			success: function () {
+			data: "txtmail="+email,
+			success: function(){
 				alert("Email com senha enviado! Cheque sua caixa de email");
 				$(this).delay(5000).location.reload();
 			},
-			error: function () {
+			error: function(){
 				alert("Não foi possível enviar o email: " + info.status + " - " + info.statusText);
-
+			
 			}
 		})
 	}
--- CDRD/WebContent/resources/js/jogo.js
+++ CDRD/WebContent/resources/js/jogo.js
@@ -2,51 +2,6 @@
 /*           Contos de Reinos Distantes                        */
 /*-------------------------------------------------------------*/
 
-class sceneMenuPrincipal extends Phaser.Scene {
-
-    constructor ()
-    {
-        super({ key: 'sceneMenuPrincipal' });
-    }
-
-    init(){
-
-    }
-
-    preload ()
-    {
-        this.load.image('bg', '../../resources/style/images/jogo/bgmenu.png');
-    }
-
-    create ()
-    {
-        this.bg = this.add.image(game.width, game.height, 'bg').setOrigin(0, 0);
-
-        this.input.manager.enabled = true;
-
-        const ButtonB = this.add.text(280, 270, 'Jogar', {fontSize:'50px', fill: '#3333ff', fontFamily: 'pixel font'});
-        ButtonB.setInteractive();
-
-        ButtonB.on('pointerdown', function(){
-            console.log('From SceneC to SceneA');
-            this.scene.start('sceneMain')
-        },this);
-
-        const ButtonC = this.add.text(620, 270, 'Ranking', {fontSize:'50px', fill: '#3333ff', fontFamily: 'pixel font' });
-        ButtonC.setInteractive();
-
-        ButtonC.on('pointerdown', function(){
-            this.scene.start('sceneMain')
-        },this);
-    }
-
-    update ()
-    {
-        
-    }
-
-}
-
 class SceneMain extends Phaser.Scene {
 
     constructor ()
@@ -60,61 +15,12 @@ class SceneMain extends Phaser.Scene {
 
     preload ()
     {
-        var barraCarregamento = this.add.graphics();
-
-        this.load.image('logo', '../../resources/style/images/jogo/test.jpg');
-        for (var i = 0; i < 500; i++) {
-            this.load.image('logo'+i, '../../resources/style/images/jogo/test.jpg');
-        }
-
-        this.load.on('progress', function (value) {
-            console.log(value);
-            barraCarregamento.clear();
-            barraCarregamento.fillStyle(0x37ac26, 1);
-            barraCarregamento.fillRect(320, 280, 300 * value, 30);
-            percentText.setText(parseInt(value * 100) + '%');
-        });
-                    
-        this.load.on('fileprogress', function (file) {
-            console.log(file.src);
-        });
-         
-        this.load.on('complete', function () {
-            console.log('complete');
-            barraCarregamento.destroy();
-
-            textoCarregando.destroy();
-            percentText.destroy();
-        });
-
-        var width = this.cameras.main.width;
-        var height = this.cameras.main.height;
-        var textoCarregando = this.make.text({
-            x: width / 2,
-            y: height / 2 - 50,
-            text: 'Carregando...',
-            style: {
-                font: '20px monospace',
-                fill: '#ffffff'
-            }
-        });
-        textoCarregando.setOrigin(0.5, 0.5);
-
-        var percentText = this.make.text({
-            x: width / 2,
-            y: height / 2 - 5,
-            text: '0%',
-            style: {
-                font: '18px monospace',
-                fill: '#ffffff'
-            }
-        });
-        percentText.setOrigin(0.5, 0.5);
+        
     }
 
     create ()
     {
-        var logo = this.add.image(400, 300, 'logo');
+        
     }
 
     update ()
@@ -128,7 +34,13 @@ var config = {
     type: Phaser.AUTO,
     width: 960,
     height: 540,
-    scene: [ sceneMenuPrincipal, SceneMain ]
+    physics: {
+      default: 'arcade',
+      arcade: {
+          debug: false
+      }
+  },
+    scene: [ SceneMain ]
 };
 
 var game = new Phaser.Game(config);
