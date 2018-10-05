bdd193af615984277f0c2bf28a51518de1235f1c
Kalyl Henings <kalylhenings@gmail.com>
2018-10-05 16:54:34 -0300

    atualização do bramch arrumado pelo wazousky

--- CDRD/WebContent/resources/js/admin.js
+++ CDRD/WebContent/resources/js/admin.js
@@ -4,178 +4,191 @@
 //------------------------------Geral--------------------------
 
 //Função para fazer as tabs do jQuery UI
-$( function() {
-    $( "#admPaginas" ).tabs();
-  } );
-
-$(function (){
+$(function() {
+  $("#admPaginas").tabs();
 });
+
+$(function() {});
 
 //-----------------------------Gerenciar Contas-----------------
 
-  
-	
-	$(function escolheUsuario(){
-		$("#listaAdm").on('click','li',function (){//Função para passar o nome da lista de usuários para o campo de deletar usuário.
-	            $("input[name=txtAdm]").val($(this).text());
-	            $("input[type=hidden][name=hdAdm]").val($(this).val());
-	    })
-	});
-	
-	$(function escolheUsuario(){
-		$("#listaJog").on('click','li',function (){//Função para passar o nome da lista de usuários para o campo de deletar usuário.
-	            $("input[name=txtJog]").val($(this).text());
-	            $("input[type=hidden][name=hdJog]").val($(this).val());
-	    })
-	});
-  
-  //se os campos estao preenchidos pede confirmação, se sim, envia.
+$(function escolheUsuario() {
+  $("#listaAdm").on("click", "li", function() {
+    //Função para passar o nome da lista de usuários para o campo de deletar usuário.
+    $("input[name=txtAdm]").val($(this).text());
+    $("input[type=hidden][name=hdAdm]").val($(this).val());
+  });
+});
 
-function validaDel(q){
-	conf=false;
-		if($("input[name=txt"+q+"").val()!=""){
-			if($("textarea[name=txamotivo"+q+"]").val()!=""){	 
-				conf = confirm("Você tem certeza que deseja deletar o usuario? (um "+q+")");
-		  	}else{
-		  		alert("Escreva o motivo para banimento.")
-		  		$("textarea[name=txamotivo"+q+"]").focus();
-		  	}
-		}else{
-			alert("Selecione um usuário primeiro.");
-			$("input[name=txt"+q+"").focus();
-		}
-	return conf;	
+$(function escolheUsuario() {
+  $("#listaJog").on("click", "li", function() {
+    //Função para passar o nome da lista de usuários para o campo de deletar usuário.
+    $("input[name=txtJog]").val($(this).text());
+    $("input[type=hidden][name=hdJog]").val($(this).val());
+  });
+});
+
+//se os campos estao preenchidos pede confirmação, se sim, envia.
+
+function validaDel(q) {
+  conf = false;
+  if ($("input[name=txt" + q + "").val() != "") {
+    if ($("textarea[name=txamotivo" + q + "]").val() != "") {
+      conf = confirm(
+        "Você tem certeza que deseja deletar o usuario? (um " + q + ")"
+      );
+    } else {
+      alert("Escreva o motivo para banimento.");
+      $("textarea[name=txamotivo" + q + "]").focus();
+    }
+  } else {
+    alert("Selecione um usuário primeiro.");
+    $("input[name=txt" + q + "").focus();
+  }
+  return conf;
 }
 
-  
 //----------------------------------Notificacoes-------------------------------------
 
-  $(function(){ //Esconde a div de editar notificacoes.
-      $("#divEditarNotificacoes").hide();
-  })
-  //Função para quando o usuário clicar em editar, descarregar a div ComporNotificacoes e carregar
-  //a div EditarNotificacoes no lugar.                
-  /*function editarNotificacao(){
+$(function() {
+  //Esconde a div de editar notificacoes.
+  $("#divEditarNotificacoes").hide();
+});
+//Função para quando o usuário clicar em editar, descarregar a div ComporNotificacoes e carregar
+//a div EditarNotificacoes no lugar.
+/*function editarNotificacao(){
       $(".divComporNotificacoes").hide();
       $("#divEditarNotificacoes").show();	 ESTA FUNCAO AGORA ESTA NO ADMINSERVLET, NO MÉTODO alterarNotificacao
-  }*/ 
-  //Funcao para descarregar EditarNotificacoes e recarregar ComporNotificacoes quando clicar em cancelar
-  function cancelarEdicao(){
-      $(".divComporNotificacoes").show();
-      $("#divEditarNotificacoes").hide();
-  }
+  }*/
 
-  function validaNotificacao(compedit){//Recebe o parâmetro se o foi chamado pelo compor ou pelo editar.
-    var conf = false;
-    if(compedit==0){
-        if($("textarea[name=txacompnotificacao]").val()!=""){
-            conf = confirm("Você tem certeza que deseja postar uma notificação?");
-            if(conf==true){
-            	insereNotificacao();
-            }
-        }else{
-            alert("Escreva uma notificação.");
-            $("textarea[name=txacompnotificacao]").focus();
-        }
-    }else if(compedit==1){
-        if($("textarea[name=txaeditnotificacao]").val()!=""){
-            conf = confirm("Você tem certeza que deseja editar uma notificação?");
-            if(conf==true){
-            	alteraNotificacao();
-            }
-        }else{
-            alert("Escreva uma notificação.");
-            $("textarea[name=txaeditnotificacao]").focus();
-        }
+//Funcao para descarregar EditarNotificacoes e recarregar ComporNotificacoes quando clicar em cancelar
+function cancelarEdicao() {
+  $(".divComporNotificacoes").show();
+  $("#divEditarNotificacoes").hide();
+}
+
+function validaNotificacao(compedit) {
+  //Recebe o parâmetro se o foi chamado pelo compor ou pelo editar.
+  var conf = false;
+  if (compedit == 0) {
+    if ($("textarea[name=txacompnotificacao]").val() != "") {
+      conf = confirm("Você tem certeza que deseja postar uma notificação?");
+      if (conf == true) {
+        insereNotificacao();
+      }
+    } else {
+      alert("Escreva uma notificação.");
+      $("textarea[name=txacompnotificacao]").focus();
     }
-    //return conf;
+  } else if (compedit == 1) {
+    if ($("textarea[name=txaeditnotificacao]").val() != "") {
+      conf = confirm("Você tem certeza que deseja editar uma notificação?");
+      if (conf == true) {
+        alteraNotificacao();
+      }
+    } else {
+      alert("Escreva uma notificação.");
+      $("textarea[name=txaeditnotificacao]").focus();
+    }
+  }
+  //return conf;
 }
 
 //--------------------------------Gerenciar Admins--------------------------------------------
 
-function validaCadastroAdm(){
-    var conf = false;
-    if($("input[name=txtnomeadm]").val()!=""){
-        if($("input[name=txtemailadm]").val()!=""){
-            if($("input[name=dtenascimentoadm]").val()!=""){
-                if($("input[name=txtapelidoadm]").val()!=""){
-                    if($("input[name=pwdsenhaadm]").val()!=""){
-                        if($("input[name=pwdconfsenhaadm]").val()!=""){
-                            if($("input[name=pwdsenhaadm]").val()==$("input[name=pwdconfsenhaadm]").val()){
-                                conf = confirm("Tem certeza que deseja cadastrar um novo administrador?");
-                            }else{
-                                alert("As senhas não coincidem.");
-                            }
-                        }else{
-                            alert("Preencha a confirmação de senha.");
-                            $("input[name=pwdconfsenhaadm]").focus();
-                        }
-                    }else{
-                        alert("Preencha a senha.");
-                        $("input[name=pwdsenhaadm]").focus();
-                    }
-                }else{
-                    alert("Preencha o nome de usuário do administrador.");
-                    $("input[name=txtapelidoadm]").focus();
-                }
-            }else{
-                alert("Preencha a data de nascimento.");
-                $("input[name=dtenascimentoadm]").focus();
+function validaCadastroAdm() {
+  var conf = false;
+  if ($("input[name=txtnomeadm]").val() != "") {
+    if ($("input[name=txtemailadm]").val() != "") {
+      if ($("input[name=dtenascimentoadm]").val() != "") {
+        if ($("input[name=txtapelidoadm]").val() != "") {
+          if ($("input[name=pwdsenhaadm]").val() != "") {
+            if ($("input[name=pwdconfsenhaadm]").val() != "") {
+              if (
+                $("input[name=pwdsenhaadm]").val() ==
+                $("input[name=pwdconfsenhaadm]").val()
+              ) {
+                conf = confirm(
+                  "Tem certeza que deseja cadastrar um novo administrador?"
+                );
+              } else {
+                alert("As senhas não coincidem.");
+              }
+            } else {
+              alert("Preencha a confirmação de senha.");
+              $("input[name=pwdconfsenhaadm]").focus();
             }
-        }else{
-            alert("Preencha o e-mail.");
-            $("input[name=txtemailadm]").focus();
+          } else {
+            alert("Preencha a senha.");
+            $("input[name=pwdsenhaadm]").focus();
+          }
+        } else {
+          alert("Preencha o nome de usuário do administrador.");
+          $("input[name=txtapelidoadm]").focus();
         }
-    }else{
-        alert("Preencha o nome.");
-        $("input[name=txtnomeadm]").focus();
+      } else {
+        alert("Preencha a data de nascimento.");
+        $("input[name=dtenascimentoadm]").focus();
+      }
+    } else {
+      alert("Preencha o e-mail.");
+      $("input[name=txtemailadm]").focus();
     }
-    return conf;
-}    
+  } else {
+    alert("Preencha o nome.");
+    $("input[name=txtnomeadm]").focus();
+  }
+  return conf;
+}
 
 //-------------------------------------Minha Conta------------------------------------------
 
-function validaMinhaConta(){
-    var conf = false;
-    if($("input[name=txtaltnome]").val()!=""){
-        if($("input[name=txtalteemail]").val()!=""){
-            if($("input[name=dtealtnascimento]").val()!=""){
-                if($("input[name=txtaltlogger]").val()!=""){
-                    if($("input[name=pwdaltsenhaantiga]").val()!=""){
-                        if($("input[name=pwdaltsenhanova]").val()!=""){
-                            if($("input[name=pwdaltconfsenha]").val()!=""){
-                                if($("input[name=pwdaltsenhanova]").val()==$("input[name=pwdaltconfsenha]").val()){
-                                    conf = confirm("Tem certeza que deseja alterar informações de sua conta?");
-                                }else{
-                                    alert("As senhas não coincidem.");
-                                }
-                            }else{
-                                alert("Preencha a confirmação de senha.");
-                                $("input[name=pwdaltconfsenha]").focus();
-                            }
-                        }else{
-                            alert("Preencha a senha nova.");
-                            $("input[name=pwdaltsenhanova]").focus();
-                        }
-                    }else{
-                        alert("Preencha a senha antiga.");
-                        $("input[name=pwdaltsenhaantiga]").focus();
-                    }
-                }else{
-                    alert("Preencha o nome de usuário.");
-                    $("input[name=txtaltlogger]").focus();
+function validaMinhaConta() {
+  var conf = false;
+  if ($("input[name=txtaltnome]").val() != "") {
+    if ($("input[name=txtalteemail]").val() != "") {
+      if ($("input[name=dtealtnascimento]").val() != "") {
+        if ($("input[name=txtaltlogger]").val() != "") {
+          if ($("input[name=pwdaltsenhaantiga]").val() != "") {
+            if ($("input[name=pwdaltsenhanova]").val() != "") {
+              if ($("input[name=pwdaltconfsenha]").val() != "") {
+                if (
+                  $("input[name=pwdaltsenhanova]").val() ==
+                  $("input[name=pwdaltconfsenha]").val()
+                ) {
+                  conf = confirm(
+                    "Tem certeza que deseja alterar informações de sua conta?"
+                  );
+                } else {
+                  alert("As senhas não coincidem.");
                 }
-            }else{
-                alert("Preencha a data de nascimento.");
-                $("input[name=dtealtnascimento]").focus();
+              } else {
+                alert("Preencha a confirmação de senha.");
+                $("input[name=pwdaltconfsenha]").focus();
+              }
+            } else {
+              alert("Preencha a senha nova.");
+              $("input[name=pwdaltsenhanova]").focus();
             }
-        }else{
-            alert("Preencha o e-mail.");
-            $("input[name=txtaltemail]").focus();
+          } else {
+            alert("Preencha a senha antiga.");
+            $("input[name=pwdaltsenhaantiga]").focus();
+          }
+        } else {
+          alert("Preencha o nome de usuário.");
+          $("input[name=txtaltlogger]").focus();
         }
-    }else{
-        alert("Preencha o nome.");
-        $("input[name=txtaltnome]").focus();
+      } else {
+        alert("Preencha a data de nascimento.");
+        $("input[name=dtealtnascimento]").focus();
+      }
+    } else {
+      alert("Preencha o e-mail.");
+      $("input[name=txtaltemail]").focus();
     }
-    return conf;
+  } else {
+    alert("Preencha o nome.");
+    $("input[name=txtaltnome]").focus();
+  }
+  return conf;
 }
--- CDRD/WebContent/resources/js/inicio.js
+++ CDRD/WebContent/resources/js/inicio.js
@@ -4,173 +4,180 @@
  * Descrição: Arquivo js relacionado à index.html (raiz do projeto). Contem funções de carregamento
  * 			  das janelas e validação dos forms da página
  */
-$(document).ready(function () {
+$(document).ready(function() {
+  //interação com a porta
+  $("#containerPortaIndex").on("mouseenter", function() {
+    $("#portaIndex").attr(
+      "src",
+      "resources/style/images/gif/portaExternaTavernaAberta.gif"
+    );
+  });
+  $("#containerPortaIndex").on("mouseleave", function() {
+    $("#portaIndex").attr(
+      "src",
+      "resources/style/images/estatico/portaExternaTavernaFechada.png"
+    );
+  });
 
-    //interação com a porta
-    $("#containerPortaIndex").on("mouseenter", function () {
-        $("#portaIndex").attr("src", "resources/style/images/gif/portaExternaTavernaAberta.gif");
-    });
-    $("#containerPortaIndex").on("mouseleave", function () {
-        $("#portaIndex").attr("src", "resources/style/images/estatico/portaExternaTavernaFechada.png");
-    });
+  //carregamento das janelas de interação
+  $("#containerModaisIndex").hide();
 
-    //carregamento das janelas de interação
-    $("#containerModaisIndex").hide();
+  var ua = detect.parse(navigator.userAgent);
 
-    var ua = detect.parse(navigator.userAgent);
+  if (ua.browser.family == "Chrome") {
+    $("html").css("image-rendering", "pixelated");
+  } else if (ua.browser.family == "Firefox") {
+    $("html").css("image-rendering", "-moz-crisp-edges");
+  } else {
+    alert(
+      "Infelizmente o navegador " +
+        ua.browser.family +
+        " não é suportado por nossa aplicação. Lamentamos o inconveninete ( ; _ ; ). Atualmente, somente o Chrome e Firefox são suportados."
+    );
+  }
 
-    if (ua.browser.family == "Chrome") {
-        $("html").css("image-rendering", "pixelated");
-    } else if (ua.browser.family == "Firefox") {
-        $("html").css("image-rendering", "-moz-crisp-edges");
-    } else {
-        alert("Infelizmente o navegador " + ua.browser.family + " não é suportado por nossa aplicação. Lamentamos o inconveninete ( ; _ ; ). Atualmente, somente o Chrome e Firefox são suportados.");
-    }
-
-    carregaModais()
+  carregaModais();
 });
 
 /**
- * função que provê um display às janelas modais, abrindo-o ou 
+ * função que provê um display às janelas modais, abrindo-o ou
  * fechando-o conforme interação do usuário com a página
  */
 function abrirModal() {
-    $("#containerModaisIndex").show();
+  $("#containerModaisIndex").show();
 
-    //Background e bloqueio de ações do visitante senão as da janela 
-    $("#paredeTaverna").css("filter", "brightness(50%)");
-    $("#muroTaverna").css("filter", "brightness(50%)");
-    $("#paredeTaverna").css("pointer-events", "none");
-    $("#muroTaverna").css("pointer-events", "none");
+  //Background e bloqueio de ações do visitante senão as da janela
+  $("#paredeTaverna").css("filter", "brightness(50%)");
+  $("#muroTaverna").css("filter", "brightness(50%)");
+  $("#paredeTaverna").css("pointer-events", "none");
+  $("#muroTaverna").css("pointer-events", "none");
 
-    carregaModais()
-    $("#intPerso").show();
-    $("#guard_1").show();
+  carregaModais();
+  $("#intPerso").show();
+  $("#guard_1").show();
 }
 
 /**
- * Funcção que realiza o controle das janelas, ordenando a sequência 
+ * Funcção que realiza o controle das janelas, ordenando a sequência
  * de abertura e fechamento
  */
 function carregaModais() {
-
-    /* Sair */
-    $(".sairBtn").click(function () {
-
-        $("#intPerso").hide();
-        $("#guard_1").hide();
-        $("#login").hide();
-        $("#registrar").hide();
-        $("#login_redes").hide();
-        $("#esqueci_senha").hide();
-        $("#containerModaisIndex").hide();
-
-        //Retira o background e reativa os eventos da página
-        $("#paredeTaverna").css("filter", "brightness(100%)");
-        $("#muroTaverna").css("filter", "brightness(100%)");
-        $("#paredeTaverna").css("pointer-events", "auto");
-        $("#paredeTaverna").css("pointer-events", "auto");
-    });
-
-    /* Login */
+  /* Sair */
+  $(".sairBtn").click(function() {
+    $("#intPerso").hide();
+    $("#guard_1").hide();
     $("#login").hide();
-    $(".loginBtn").click(function () {
-        $("#guard_1").hide();
-        $("#login_redes").hide();
-        $("#registrar").hide();
-        $("#esqueci_senha").hide();
-        $("#login").show();
-    });
-
-    /*Redes Sociais*/
-    $("#login_redes").hide();
-    $(".redesBtn").click(function () {
-        $("#guard_1").hide();
-        $("#login").hide();
-        $("#login_redes").show();
-    });
-
-    /* Registrar */
     $("#registrar").hide();
-    $(".registrarBtn").click(function () {
-        $("#guard_1").hide();
-        $("#login").hide();
-        $("#registrar").show();
-    })
-
-    /* Esqueci Senha */
+    $("#login_redes").hide();
     $("#esqueci_senha").hide();
-    $(".esqueciBtn").click(function () {
-        $("#login").hide();
-        $("#esqueci_senha").show();
-    });
+    $("#containerModaisIndex").hide();
+
+    //Retira o background e reativa os eventos da página
+    $("#paredeTaverna").css("filter", "brightness(100%)");
+    $("#muroTaverna").css("filter", "brightness(100%)");
+    $("#paredeTaverna").css("pointer-events", "auto");
+    $("#paredeTaverna").css("pointer-events", "auto");
+  });
+
+  /* Login */
+  $("#login").hide();
+  $(".loginBtn").click(function() {
+    $("#guard_1").hide();
+    $("#login_redes").hide();
+    $("#registrar").hide();
+    $("#esqueci_senha").hide();
+    $("#login").show();
+  });
+
+  /*Redes Sociais*/
+  $("#login_redes").hide();
+  $(".redesBtn").click(function() {
+    $("#guard_1").hide();
+    $("#login").hide();
+    $("#login_redes").show();
+  });
+
+  /* Registrar */
+  $("#registrar").hide();
+  $(".registrarBtn").click(function() {
+    $("#guard_1").hide();
+    $("#login").hide();
+    $("#registrar").show();
+  });
+
+  /* Esqueci Senha */
+  $("#esqueci_senha").hide();
+  $(".esqueciBtn").click(function() {
+    $("#login").hide();
+    $("#esqueci_senha").show();
+  });
 }
 
 /*=======================================Validação de formulários=======================================*/
 function validaLogin() {
-
-    if ($("input[name=txtlogin]").val() != "") {
-        if ($("input[name=pwdsenhalogger]").val() != "") {
-
-            login();
-
-        } else {
-            alert('Preencha o campo Senha');
-            $("input[name=pwdsenhalogger]").focus();
-        }
+  if ($("input[name=txtlogin]").val() != "") {
+    if ($("input[name=pwdsenhalogger]").val() != "") {
+      login();
     } else {
-        alert('Preencha o campo "Usuário"');
-        $("input[name=txtlogin]").focus();
+      alert("Preencha o campo Senha");
+      $("input[name=pwdsenhalogger]").focus();
     }
+  } else {
+    alert('Preencha o campo "Usuário"');
+    $("input[name=txtlogin]").focus();
+  }
 }
 
 function validaCadastro() {
-
-    if ($("input[name=txtnome]").val() != "") {
-        if ($("input[name=txtemail]").val() != "") {
-            if ($("input[name=dtenascimento]").val() != "") {
-                if ($("input[name=txtapelido]").val() != "") {
-                    if ($("input[name=pwdsenha]").val() != "") {
-                        if ($("input[name=pwdconfsenha]").val() != "") {
-                            if ($("input[name=pwdsenha]").val() == $("input[name=pwdconfsenha]").val()) {
-                                cadastrar();
-                            } else {
-                                alert('O campo "Confirmar Senha" deve ser igual ao campo "Senha"');
-                                $("input[name=pwdconfsenha]").focus();
-                            }
-                        } else {
-                            alert('Preencha o campo "Confirmar Senha"');
-                            $("input[name=pwdconfsenha]").focus();
-                        }
-                    } else {
-                        alert('Preencha o campo "Senha"');
-                        $("input[name=pwdsenha]").focus();
-                    }
-                } else {
-                    alert('Preencha o campo "Usuário"');
-                    $("input[name=txtname]").focus();
-                }
+  if ($("input[name=txtnome]").val() != "") {
+    if ($("input[name=txtemail]").val() != "") {
+      if ($("input[name=dtenascimento]").val() != "") {
+        if ($("input[name=txtapelido]").val() != "") {
+          if ($("input[name=pwdsenha]").val() != "") {
+            if ($("input[name=pwdconfsenha]").val() != "") {
+              if (
+                $("input[name=pwdsenha]").val() ==
+                $("input[name=pwdconfsenha]").val()
+              ) {
+                cadastrar();
+              } else {
+                alert(
+                  'O campo "Confirmar Senha" deve ser igual ao campo "Senha"'
+                );
+                $("input[name=pwdconfsenha]").focus();
+              }
             } else {
-                alert('Preencha o campo "Data de Nascimento"');
-                $("input[name=dtenascimento]").focus();
+              alert('Preencha o campo "Confirmar Senha"');
+              $("input[name=pwdconfsenha]").focus();
             }
+          } else {
+            alert('Preencha o campo "Senha"');
+            $("input[name=pwdsenha]").focus();
+          }
         } else {
-            alert('Preencha o campo "E-mail"');
-            $("input[name=txtemail]").focus();
+          alert('Preencha o campo "Usuário"');
+          $("input[name=txtname]").focus();
         }
+      } else {
+        alert('Preencha o campo "Data de Nascimento"');
+        $("input[name=dtenascimento]").focus();
+      }
     } else {
-        alert('Preencha o campo "Nome');
-        $("input[name=txtnome]").focus();
+      alert('Preencha o campo "E-mail"');
+      $("input[name=txtemail]").focus();
     }
+  } else {
+    alert('Preencha o campo "Nome');
+    $("input[name=txtnome]").focus();
+  }
 }
 
 function validaEsqueciSenha() {
-
-    if ($("input[name=txtmail]").val() != "") {
-        recuperarSenha();
-    } else {
-        alert('Preencha o campo "Endereço de E-mail"');
-        $("input[name=txtmail]").focus();
-    }
-    ''
+  if ($("input[name=txtmail]").val() != "") {
+    recuperarSenha();
+  } else {
+    alert('Preencha o campo "Endereço de E-mail"');
+    $("input[name=txtmail]").focus();
+  }
+  ("");
 }
